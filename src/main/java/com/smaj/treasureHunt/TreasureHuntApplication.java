2c645982250f2cb97f0356879e0faf3e58ff242b
Samah Majadla <smajadla@smajadla-a02.vmware.com>
2022-07-10 23:04:56 -0400

    security

--- src/main/java/com/smaj/treasureHunt/TreasureHuntApplication.java
+++ src/main/java/com/smaj/treasureHunt/TreasureHuntApplication.java
@@ -2,6 +2,10 @@ package com.smaj.treasureHunt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class TreasureHuntApplication {
@@ -9,5 +13,13 @@ public class TreasureHuntApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TreasureHuntApplication.class, args);
 	}
-
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/products").allowedOrigins("*");
+			}
+		};
+	}
 }

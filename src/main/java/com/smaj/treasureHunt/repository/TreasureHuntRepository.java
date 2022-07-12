236bba7483b9aab75dc763e6e7a72a852a9a7c40
Samah Majadla <smajadla@smajadla-a02.vmware.com>
2022-07-12 11:11:31 -0400

    added repo tag

--- src/main/java/com/smaj/treasureHunt/repository/TreasureHuntRepository.java
+++ src/main/java/com/smaj/treasureHunt/repository/TreasureHuntRepository.java
@@ -2,6 +2,8 @@ package com.smaj.treasureHunt.repository;
 
 import com.smaj.treasureHunt.model.TreasureHunt;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TreasureHuntRepository extends CrudRepository<TreasureHunt, Long> {
 }
--- src/main/java/com/smaj/treasureHunt/repository/TreasureHuntStepRepository.java
+++ src/main/java/com/smaj/treasureHunt/repository/TreasureHuntStepRepository.java
@@ -2,9 +2,11 @@ package com.smaj.treasureHunt.repository;
 
 import com.smaj.treasureHunt.model.TreasureHuntStep;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface TreasureHuntStepRepository extends CrudRepository<TreasureHuntStep, Long> {
     void deleteAllByTreasureHuntId(long treasureHuntId);
 

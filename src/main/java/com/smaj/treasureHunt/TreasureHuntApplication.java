988b00a09ac5102d545a7bf7509116493a8688f0
Samah Majadla <smajadla@smajadla-a02.vmware.com>
2022-07-13 13:52:23 -0400

    forcing web context

--- src/main/java/com/smaj/treasureHunt/TreasureHuntApplication.java
+++ src/main/java/com/smaj/treasureHunt/TreasureHuntApplication.java
@@ -1,13 +1,18 @@
 package com.smaj.treasureHunt;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 
 @SpringBootApplication
 public class TreasureHuntApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TreasureHuntApplication.class, args);
+
+		SpringApplication app = new SpringApplication(TreasureHuntApplication.class);
+		app.setApplicationContextFactory((type)-> new AnnotationConfigWebApplicationContext());
+		app.run(args);
 	}
 
 }

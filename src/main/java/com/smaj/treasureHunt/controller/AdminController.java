298069438d06ca401a0acb7230e29caa7c41a0a9
Samah Majadla <smajadla@smajadla-a02.vmware.com>
2022-07-13 23:28:23 -0400

    password check

--- src/main/java/com/smaj/treasureHunt/controller/AdminController.java
+++ src/main/java/com/smaj/treasureHunt/controller/AdminController.java
@@ -3,11 +3,9 @@ package com.smaj.treasureHunt.controller;
 import com.smaj.treasureHunt.model.Admin;
 import com.smaj.treasureHunt.service.AdminService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin
 @RestController
@@ -24,4 +22,15 @@ public class AdminController {
     public ResponseEntity createAdmin(@RequestBody Admin admin){
         return ResponseEntity.ok(adminService.addAdmin(admin));
     }
+
+    @PostMapping("/login")
+    public ResponseEntity login(@RequestParam String password){
+        boolean isAdmin =  adminService.isAdmin(password);
+        if (isAdmin) {
+            return ResponseEntity.ok().build();
+        }else{
+            return new ResponseEntity(HttpStatus.UNAUTHORIZED);
+        }
+    }
+
 }
--- src/main/java/com/smaj/treasureHunt/service/AdminService.java
+++ src/main/java/com/smaj/treasureHunt/service/AdminService.java
@@ -5,6 +5,10 @@ import com.smaj.treasureHunt.repository.AdminRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+
+import static java.lang.Long.valueOf;
+
 @Service
 public class AdminService {
 
@@ -18,5 +22,15 @@ public class AdminService {
 
     public Admin addAdmin(Admin admin){
         return adminRepository.save(admin);
+    }
+
+    public boolean isAdmin(String password){
+        Admin admin = adminRepository.findById(valueOf(10)).get();
+        if (password == admin.getPassword()){
+            return true;
+        } else {
+            return false;
+        }
+
     }
 }

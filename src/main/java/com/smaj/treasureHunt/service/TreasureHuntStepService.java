a40aa865976e107b5a909d96dd6332e430a6a260
Samah Majadla <smajadla@smajadla-a02.vmware.com>
2022-07-23 11:35:03 -0400

    logic for step incrementation

--- src/main/java/com/smaj/treasureHunt/service/TreasureHuntStepService.java
+++ src/main/java/com/smaj/treasureHunt/service/TreasureHuntStepService.java
@@ -54,7 +54,7 @@ public class TreasureHuntStepService {
             treasureHuntStep.setCompletionStatus(CompletionStatus.COMPLETED);
             TreasureHunt treasureHunt = treasureHuntStep.getTreasureHunt();
 
-            if (treasureHunt.getSteps().size()< treasureHuntStep.getOrderNumber()-1){
+            if (treasureHunt.getSteps().size()> treasureHuntStep.getOrderNumber()-1){
                 treasureHunt.setCurrentStep(treasureHunt.getCurrentStep()+1);
             }
             treasureHuntStepRepository.save(treasureHuntStep);
